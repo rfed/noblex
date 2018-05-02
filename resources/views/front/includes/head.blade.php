334c4d6a0c8ed6841f0c6bcae62b6ee0a0f4a4cb
Damian <dam@Damians-MacBook-Pro.local>
2018-05-02 14:09:43 -0300

    Img share

--- resources/views/front/includes/head.blade.php
+++ resources/views/front/includes/head.blade.php
@@ -20,11 +20,11 @@
 
 	<meta property="og:title" content="Noblex">
 	<meta property="og:site_name" content="Noblex">
-	<meta property="og:url" content="{{ url() }}">
+	<meta property="og:url" content="{{ url('') }}">
 	<meta property="og:description" content="">
 	<meta property="fb:app_id" content="221366011964601">
 	<meta property="og:type" content="website">
-	<meta property="og:image" content="{{ asset('assets/imgs/iconos/share-web.png }}">
+	<meta property="og:image" content="{{ asset('assets/imgs/iconos/share-web.png') }}">
 
 	@stack('styles')
 </head>
